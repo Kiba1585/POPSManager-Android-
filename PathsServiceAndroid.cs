2614b9bb54c283942d3121c3244bbde4a9658ab1
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-13 21:30:14 -0400

    Create PathsServiceAndroid.cs

---
+++ PathsServiceAndroid.cs
@@ -0,0 +1,16 @@
+using POPSManager.Core.Services;
+
+namespace POPSManager.Android;
+
+public class PathsServiceAndroid : IPathsService
+{
+    public async Task<string?> SelectFolderAsync()
+    {
+        var result = await FilePicker.Default.PickAsync(new PickOptions
+        {
+            PickerTitle = "Seleccione una carpeta"
+        });
+
+        return result?.FullPath;
+    }
+}
