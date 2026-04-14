3b61c4a98c8ee6976b69cfc911d2d1445eafbd3e
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-13 22:09:38 -0400

    Create GameInfo.cs

---
+++ POPSManager.Core/Models/GameInfo.cs
@@ -0,0 +1,8 @@
+namespace POPSManager.Core.Models;
+
+public class GameInfo
+{
+    public string Name { get; set; } = "";
+    public string Path { get; set; } = "";
+    public string Id { get; set; } = "";
+}
