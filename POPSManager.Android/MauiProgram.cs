f4674a5eaafbde0c4e69e5ea9cde5e3e1a482ae5
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-18 11:12:41 -0400

    Update MauiProgram.cs

--- POPSManager.Android/MauiProgram.cs
+++ POPSManager.Android/MauiProgram.cs
@@ -1,6 +1,16 @@
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+
+// Android services
+using POPSManager.Android.Services;
+
+// Core services
 using POPSManager.Core.Services;
+using POPSManager.Core;
+
+// Views & ViewModels
+using POPSManager.Android.Views;
+using POPSManager.Android.ViewModels;
 
 namespace POPSManager.Android;
 
@@ -11,14 +21,19 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
 
         builder
-            .UseMauiApp<App>();
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-        // Servicios Android
+        // Android-specific services
         builder.Services.AddSingleton<IPathsService, PathsServiceAndroid>();
         builder.Services.AddSingleton<ILoggingService, LoggingServiceAndroid>();
         builder.Services.AddSingleton<INotificationService, NotificationServiceAndroid>();
 
-        // Core
+        // Core services
         builder.Services.AddSingleton<GameProcessor>();
         builder.Services.AddSingleton<ConverterService>();
 
