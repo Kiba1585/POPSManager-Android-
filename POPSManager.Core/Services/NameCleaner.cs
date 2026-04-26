42743c5650c4c1b61f04272c6450e2d1bb54c464
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-26 15:09:33 -0400

    Actualizar NameCleaner.cs

--- POPSManager.Core/Services/NameCleaner.cs
+++ POPSManager.Core/Services/NameCleaner.cs
@@ -1,18 +1,17 @@
+using System.Globalization;
+
 namespace POPSManager.Core.Services;
 
 public static class NameCleaner
 {
     public static string Clean(string name)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            return name;
+        if (string.IsNullOrWhiteSpace(name)) return name;
 
         name = name.Replace("_", " ")
                    .Replace("-", " ")
                    .Trim();
 
-        return System.Globalization.CultureInfo.CurrentCulture
-            .TextInfo
-            .ToTitleCase(name.ToLower());
+        return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name.ToLower());
     }
 }
