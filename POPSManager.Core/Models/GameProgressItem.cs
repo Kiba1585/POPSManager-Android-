f6f6fac9990035024054c3f8e05591721389958b
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-26 14:56:28 -0400

    Actualizar GameProgressItem.cs

--- POPSManager.Core/Models/GameProgressItem.cs
+++ POPSManager.Core/Models/GameProgressItem.cs
@@ -1,7 +1,26 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace POPSManager.Core.Models;
 
-public class GameProgressItem
+public class GameProgressItem : INotifyPropertyChanged
 {
-    public string Name { get; set; } = "";
-    public string Status { get; set; } = "Pendiente";
+    private string _name = "";
+    private string _status = "Pendiente";
+
+    public string Name
+    {
+        get => _name;
+        set { _name = value; OnPropertyChanged(); }
+    }
+
+    public string Status
+    {
+        get => _status;
+        set { _status = value; OnPropertyChanged(); }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
