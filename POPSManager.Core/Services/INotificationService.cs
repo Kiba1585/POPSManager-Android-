aeda6bd3b36a27e8a1eef3e46d3dfa9c86682d41
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-26 15:02:31 -0400

    Actualizar INotificationService.cs

--- POPSManager.Core/Services/INotificationService.cs
+++ POPSManager.Core/Services/INotificationService.cs
@@ -1,6 +1,12 @@
+using POPSManager.Core.Models;
+
 namespace POPSManager.Core.Services;
 
 public interface INotificationService
 {
-    void Show(string message);
+    void Show(string message, NotificationType type);
+    void Success(string message);
+    void Error(string message);
+    void Warning(string message);
+    void Info(string message);
 }
