2aa1c7af945d714973dc1c94feac5ff1d4553ece
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-26 15:02:11 -0400

    Actualizar ILoggingService.cs

--- POPSManager.Core/Services/ILoggingService.cs
+++ POPSManager.Core/Services/ILoggingService.cs
@@ -3,4 +3,7 @@ namespace POPSManager.Core.Services;
 public interface ILoggingService
 {
     void Log(string message);
+    void Info(string msg);
+    void Warn(string msg);
+    void Error(string msg);
 }
