e0f09530edc0baf042ba23a2fb3b6c17d5da0977
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-26 15:02:50 -0400

    Actualizar IPathsService.cs

--- POPSManager.Core/Services/IPathsService.cs
+++ POPSManager.Core/Services/IPathsService.cs
@@ -2,5 +2,19 @@ namespace POPSManager.Core.Services;
 
 public interface IPathsService
 {
+    string RootFolder { get; set; }
+    string PopsFolder { get; }
+    string AppsFolder { get; }
+    string CfgFolder { get; }
+    string ArtFolder { get; }
+    string DvdFolder { get; }
+    string PopstarterElfPath { get; set; }
+    string PopstarterPs2ElfPath { get; set; }
+    string TempFolder { get; }
+    
     Task<string?> SelectFolderAsync();
+    Task<string?> SelectFileAsync(string filter);
+    
+    void SetElfPath(string path);
+    void SetPs2ElfPath(string path);
 }
