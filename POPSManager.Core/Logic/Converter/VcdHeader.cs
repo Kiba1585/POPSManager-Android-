221c93740243037a8e0a08a25a301b5e7ec51c0f
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-27 03:30:07 -0400

    Actualizar VcdHeader.cs

--- POPSManager.Core/Logic/Converter/VcdHeader.cs
+++ POPSManager.Core/Logic/Converter/VcdHeader.cs
@@ -2,28 +2,22 @@ using System;
 using System.IO;
 using System.Text;
 
-namespace POPSManager.Logic
+namespace POPSManager.Core.Logic.Converter
 {
     public static class VcdHeader
     {
         public static void Write(FileStream output, string name, long binSize, Action<string> log)
         {
             byte[] header = new byte[0x800];
-
-            // PSX signature
             Array.Copy(Encoding.ASCII.GetBytes("PSX"), 0, header, 0, 3);
 
-            // Sector count
             int sectors = (int)(binSize / 2352);
             BitConverter.GetBytes(sectors).CopyTo(header, 0x10);
 
-            // Game title (max 32 chars)
             var label = Encoding.ASCII.GetBytes(name.ToUpperInvariant());
             Array.Copy(label, 0, header, 0x20, Math.Min(label.Length, 32));
 
-            // Write header
             output.Write(header, 0, header.Length);
-
             log("[VCD] Header escrito correctamente");
         }
     }
