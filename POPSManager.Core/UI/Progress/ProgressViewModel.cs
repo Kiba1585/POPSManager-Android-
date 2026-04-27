092f76f146f0bffda86450b3a1f963cb22d1d57e
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-27 04:10:31 -0400

    Actualizar ProgressViewModel.cs

--- POPSManager.Core/UI/Progress/ProgressViewModel.cs
+++ POPSManager.Core/UI/Progress/ProgressViewModel.cs
@@ -1,11 +1,7 @@
-using System.Collections.ObjectModel;
-
 namespace POPSManager.Core.UI.Progress
 {
     public class ProgressViewModel
     {
-        public ObservableCollection<object> Items { get; } = new();
-
         public void AddGame(string name, string id) { }
         public void UpdateStatus(string id, string status) { }
         public void MarkCompleted(string id) { }
