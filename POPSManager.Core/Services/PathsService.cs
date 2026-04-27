52b25d57b95e1f18080b2ee58c5b5adff6b78304
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-27 03:30:40 -0400

    Actualizar PathsService.cs

--- POPSManager.Core/Services/PathsService.cs
+++ POPSManager.Core/Services/PathsService.cs
@@ -2,23 +2,26 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace POPSManager.Core.Services;
-
-public class PathsService : IPathsService
+namespace POPSManager.Core.Services
 {
-    public string RootFolder { get; set; } = "";
-    public string PopsFolder => Path.Combine(RootFolder, "POPS");
-    public string AppsFolder => Path.Combine(RootFolder, "APPS");
-    public string CfgFolder => Path.Combine(RootFolder, "CFG");
-    public string ArtFolder => Path.Combine(RootFolder, "ART");
-    public string DvdFolder => Path.Combine(RootFolder, "DVD");
-    public string PopstarterElfPath { get; set; } = "";
-    public string PopstarterPs2ElfPath { get; set; } = "";
-    public string TempFolder => Path.Combine(Path.GetTempPath(), "POPSManager");
+    public class PathsService : IPathsService
+    {
+        public string RootFolder { get; set; } = "";
+        public string PopsFolder => Path.Combine(RootFolder, "POPS");
+        public string AppsFolder => Path.Combine(RootFolder, "APPS");
+        public string CfgFolder => Path.Combine(RootFolder, "CFG");
+        public string ArtFolder => Path.Combine(RootFolder, "ART");
+        public string DvdFolder => Path.Combine(RootFolder, "DVD");
+        public string LngFolder => Path.Combine(RootFolder, "LNG");
+        public string ThmFolder => Path.Combine(RootFolder, "THM");
+        public string PopstarterElfPath { get; set; } = "";
+        public string PopstarterPs2ElfPath { get; set; } = "";
+        public string TempFolder => Path.Combine(Path.GetTempPath(), "POPSManager");
 
-    public Task<string?> SelectFolderAsync() => Task.FromResult<string?>(null);
-    public Task<string?> SelectFileAsync(string filter) => Task.FromResult<string?>(null);
+        public Task<string?> SelectFolderAsync() => Task.FromResult<string?>(null);
+        public Task<string?> SelectFileAsync(string filter) => Task.FromResult<string?>(null);
 
-    public void SetElfPath(string path) => PopstarterElfPath = path;
-    public void SetPs2ElfPath(string path) => PopstarterPs2ElfPath = path;
+        public void SetElfPath(string path) => PopstarterElfPath = path;
+        public void SetPs2ElfPath(string path) => PopstarterPs2ElfPath = path;
+    }
 }
