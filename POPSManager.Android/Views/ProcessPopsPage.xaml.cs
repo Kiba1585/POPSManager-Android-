e0bb86091b29c8097f5dc6a4eac0dc7c4a6f72b4
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-28 22:24:44 -0400

    Actualizar ProcessPopsPage.xaml.cs

--- POPSManager.Android/Views/ProcessPopsPage.xaml.cs
+++ POPSManager.Android/Views/ProcessPopsPage.xaml.cs
@@ -6,7 +6,22 @@ public partial class ProcessPopsPage : ContentPage
 {
     public ProcessPopsPage(ProcessPopsViewModel vm)
     {
-        InitializeComponent();
-        BindingContext = vm;
+        try
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+        catch (Exception ex)
+        {
+            InitializeComponent();
+            BindingContext = null;
+            Content = new Label
+            {
+                Text = $"Error al cargar: {ex.Message}",
+                TextColor = Colors.Red,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+        }
     }
 }
