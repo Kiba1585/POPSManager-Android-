29ae51ce4ff58947c1838cd4dfd7e3838d601596
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-04-28 23:36:57 -0400

    Actualizar MainActivity.cs

--- POPSManager.Android/Platforms/Android/MainActivity.cs
+++ POPSManager.Android/Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Content;
+using Android.Provider;
 
 namespace POPSManager.Android;
 
@@ -15,4 +17,18 @@ namespace POPSManager.Android;
                            ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Solicitar permiso MANAGE_EXTERNAL_STORAGE en Android 11+
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+        {
+            if (!Android.OS.Environment.IsExternalStorageManager)
+            {
+                var intent = new Intent(Settings.ActionManageAllFilesAccessPermission);
+                StartActivity(intent);
+            }
+        }
+    }
 }
