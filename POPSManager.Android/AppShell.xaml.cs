7553bc0ff3fad3a506d1ad8f68b9760e956c6026
Kiba1585 <122737594+Kiba1585@users.noreply.github.com>
2026-05-01 17:01:54 -0400

    Actualizar AppShell.xaml.cs

--- POPSManager.Android/AppShell.xaml.cs
+++ POPSManager.Android/AppShell.xaml.cs
@@ -7,12 +7,5 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-
-        // Tema oscuro para el Shell
-        Shell.SetNavBarBackgroundColor(this, Color.FromArgb("#161B22"));
-        Shell.SetNavBarTitleColor(this, Color.FromArgb("#F0F6FC"));
-        Shell.SetTabBarBackgroundColor(this, Color.FromArgb("#161B22"));
-        Shell.SetTabBarForegroundColor(this, Color.FromArgb("#58A6FF"));
-        Shell.SetTabBarUnselectedColor(this, Color.FromArgb("#6E7681"));
     }
 }
